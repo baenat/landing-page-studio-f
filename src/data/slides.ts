6a1df398a1dd221bfcb86d0e62f5fab1c0901aa0
Baenat <bae.julian10@gmail.com>
2025-11-22 18:56:35 -0500

    Añade arreglo de información de los slider a visualizar con la ruta de ubicación de cada imagen

---
+++ src/data/slides.ts
@@ -0,0 +1,52 @@
+import type { Slide } from "../types";
+
+export const slides: Slide[] = [
+	{
+		id: 'slide-1',
+		title: 'SHINE ON',
+		subtitle: 'CELEBRATE THE MAGIC',
+		image: '/sliders/web/image1.png',
+		imageMobile: '/sliders/mobile/image1.png',
+		alt: 'Shine On Collection',
+		cta: {
+			text: 'SHOP NOW',
+			link: '#shop'
+		}
+	},
+	{
+		id: 'slide-2',
+		title: 'URBAN VIBES',
+		subtitle: 'NEW COLLECTION 2024',
+		image: '/sliders/web/image2.png',
+		imageMobile: '/sliders/mobile/image2.png',
+		alt: 'Urban Collection',
+		cta: {
+			text: 'DISCOVER',
+			link: '#shop'
+		}
+	},
+	{
+		id: 'slide-3',
+		title: 'TIMELESS ELEGANCE',
+		subtitle: 'EXCLUSIVE DESIGNS',
+		image: '/sliders/web/image3.png',
+		imageMobile: '/sliders/mobile/image3.png',
+		alt: 'Elegant Collection',
+		cta: {
+			text: 'EXPLORE',
+			link: '#shop'
+		}
+	},
+	{
+		id: 'slide-4',
+		title: 'MODA STUDIO F',
+		subtitle: 'EXCLUSIVE DESIGNS',
+		image: '/sliders/web/image4.png',
+		imageMobile: '/sliders/mobile/image4.png',
+		alt: 'Elegant Collection',
+		cta: {
+			text: 'EXPLORE',
+			link: '#shop'
+		}
+	}
+];
