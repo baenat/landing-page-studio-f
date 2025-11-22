92f32d77511794a2785e054084c22b0b6083909e
Baenat <bae.julian10@gmail.com>
2025-11-22 18:57:18 -0500

    Añade arreglo de categorías para el menu

---
+++ src/data/categories.ts
@@ -0,0 +1,8 @@
+import type { Category } from "../types";
+
+export const categories: Category[] = [
+	{ id: 1, name: 'JEANS', slug: 'jeans' },
+	{ id: 2, name: 'PANTS', slug: 'pants' },
+	{ id: 3, name: 'COATS', slug: 'coats' },
+	{ id: 4, name: 'BOOTS', slug: 'boots' }
+];
