5ad3c38be276095103ca57d845153d4da13f892e
Baenat <bae.julian10@gmail.com>
2025-11-23 12:05:08 -0500

    Implementa funcionalidad para visualizar sección de tallas al seleccionar el icono +

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -7,9 +7,10 @@ import { useState } from 'react';
  */
 export function ProductCard({ product, onAddClick }: ProductCardProps) {
 	const [selectedSize, setSelectedSize] = useState<string>('');
-	
+	const [showSectionCart, setShowSectionCart] = useState<boolean>(false);
+
 	return (
-		<div className="group relative bg-white">
+		<section className="group relative bg-white" onMouseLeave={() => setShowSectionCart(false)}>
 			<div className="relative aspect-2/3 overflow-hidden bg-gray-100 mb-4">
 				<img
 					src={product.image}
@@ -19,7 +20,8 @@ export function ProductCard({ product, onAddClick }: ProductCardProps) {
 
 				{/* Icono + para agregar */}
 				<button
-					onClick={() => onAddClick(product)}
+					// onClick={() => onAddClick(product)}
+					onClick={() => setShowSectionCart(true)}
 					className="absolute top-4 right-4 bg-amber-950 text-white rounded-full p-1 transition-all duration-300 z-10 cursor-pointer"
 					aria-label={`Agregar ${product.name} al carrito`}
 				>
@@ -28,28 +30,29 @@ export function ProductCard({ product, onAddClick }: ProductCardProps) {
 			</div>
 
 			{/* Size selector card (visible en hover en desktop) */}
-			<div className="absolute bottom-16 left-4 right-4 bg-white shadow-lg p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20 hidden md:block">
-				<p className="text-xs text-center mb-2 font-thin">Selecciona tu talla</p>
-				<p className="text-xs text-center mb-3 font-medium">{product.name}</p>
-				<div className="flex justify-center gap-2 mb-3 flex-wrap">
-					{product.sizes.slice(0, 6).map((size) => (
-						<button
-							key={size}
-							onClick={() => setSelectedSize(size)}
-							className={`w-8 h-8 border hover:bg-amber-900 hover:text-white transition-colors text-sm font-medium cursor-pointer ${selectedSize === size ? 'bg-amber-900 text-white' : 'bg-white text-black'}`}
-						>
-							{size}
-						</button>
-					))}
-				</div>
-				<button
-					onClick={() => onAddClick(product)}
-					className="w-full flex items-center justify-center gap-2 text-sm hover:text-amber-900 hover:font-bold transition-all cursor-pointer"
-				>
-					<ShoppingBag className="w-4 h-4" />
-					Añadir
-				</button>
-			</div>
+			{showSectionCart &&
+				<div className="absolute bottom-16 left-4 right-4 bg-white shadow-lg p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20 hidden md:block">
+					<p className="text-xs text-center mb-2 font-thin">Selecciona tu talla</p>
+					<p className="text-xs text-center mb-3 font-medium">{product.name}</p>
+					<div className="flex justify-center gap-2 mb-3 flex-wrap">
+						{product.sizes.map((size) => (
+							<button
+								key={size}
+								onClick={() => setSelectedSize(size)}
+								className={`w-8 h-8 border hover:bg-amber-900 hover:text-white transition-colors text-sm font-medium cursor-pointer ${selectedSize === size ? 'bg-amber-900 text-white' : 'bg-white text-black'}`}
+							>
+								{size}
+							</button>
+						))}
+					</div>
+					<button
+						onClick={() => onAddClick(product)}
+						className="w-full flex items-center justify-center gap-2 text-sm hover:text-amber-900 hover:font-bold transition-all cursor-pointer"
+					>
+						<ShoppingBag className="w-4 h-4" />
+						Añadir
+					</button>
+				</div>}
 
 			{/* Button */}
 			<button
@@ -58,6 +61,6 @@ export function ProductCard({ product, onAddClick }: ProductCardProps) {
 			>
 				COMPRAR
 			</button>
-		</div>
+		</section>
 	);
 }
