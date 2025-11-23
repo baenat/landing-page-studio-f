c648eb91ae56eaac87bdf7f7c7d347cd7e039c05
Baenat <bae.julian10@gmail.com>
2025-11-23 15:09:44 -0500

    Crea componente genérico para visualizar titulo por sección

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,16 @@
+interface SectionTitleProps {
+	title: string;
+}
+
+export function SectionTitle({ title }: SectionTitleProps) {
+
+	return (
+		<section className="text-center mb-16" >
+			<h2 className="text-3xl md:text-4xl font-serif text-gray-900 tracking-wide inline-block uppercase">
+				{title}
+				<div className="w-24 h-0.5 bg-gray-900 mx-auto mt-2" />
+			</h2>
+		</section >
+	)
+
+}
