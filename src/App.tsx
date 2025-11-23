aba7ab69e57358e517f3672d53c05ffc0dbff374
Baenat <bae.julian10@gmail.com>
2025-11-23 16:45:21 -0500

    Implementa sección de galleria de fotos en app principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { GallerySection } from "./components/Gallery"
 import { MomentsSection } from "./components/Moments"
 import { Navigation } from "./components/Navigation"
 import { ProductsSection } from "./components/Products"
 import { Slider } from "./components/Slider"
 import { categories } from "./data/categories"
+import { galleryImages } from "./data/gallery"
 import { moments } from "./data/moments"
 import { products } from "./data/products"
 import { slides } from "./data/slides"
@@ -15,6 +17,7 @@ function App() {
       <Slider slides={slides} autoPlay interval={5000} />
       <ProductsSection products={products} />
       <MomentsSection moments={moments} />
+      <GallerySection gallery={galleryImages} />
     </div>
   )
 }
